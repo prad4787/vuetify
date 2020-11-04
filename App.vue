135fb2c2e43b329b87a844b5e7ab26185dc3d3e4
prad4787 <prad4787@gmail.com>
2020-11-04 14:01:03 +0545

    first commit

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+// Main Stylesheet
+@import "./src/assets/scss/main";
+// Bootstrap Vue Stylesheet
+@import "~bootstrap-vue/src/index";
+// All color themes are included and available by default
+// Feel free to comment out any of them if you won't use them in your project
+@import "./src/assets/scss/oneui/themes/amethyst";
+@import "./src/assets/scss/oneui/themes/city";
+@import "./src/assets/scss/oneui/themes/flat";
+@import "./src/assets/scss/oneui/themes/modern";
+@import "./src/assets/scss/oneui/themes/smooth";
+</style>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
